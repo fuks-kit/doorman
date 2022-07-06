1064ef64a8352b95c86c4a003b5de598564a47eb
patrick <patrick@kappa-mm.de>
2022-07-06 23:21:37 +0200

    Added a debug function

--- cmd/rfid/rfid.go
+++ cmd/rfid/rfid.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"doorwatch/rfid"
+	"encoding/json"
 	"flag"
 	"log"
 )
 
 var devicePath string
+var dump bool
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	flag.StringVar(&devicePath, "d", "/dev/input/event1", "device input path")
+	flag.StringVar(&devicePath, "i", "/dev/input/event1", "device input path")
+	flag.BoolVar(&dump, "d", false, "dump all events from input device")
 	flag.Parse()
 }
 
@@ -19,6 +22,18 @@ func main() {
 	log.Printf("start reading and parsing input from %s", devicePath)
 
 	device := rfid.Reader(devicePath)
+
+	if dump {
+		log.Printf("dump all inputs")
+
+		for event := range device.StreamEvents() {
+			out, _ := json.MarshalIndent(event, "", "  ")
+			log.Printf("%s", out)
+		}
+
+		return
+	}
+
 	for id := range device.StreamIds() {
 		log.Printf("id=0x%08x (%d)", id, id)
 	}
