c8c64ab90b50ff29c47426f0aeb40b775066bd15
patrick <patrick@kappa-mm.de>
2022-07-12 18:55:27 +0200

    Refactor some stuff

--- cmd/doorman/doorman.go
+++ cmd/doorman/doorman.go
@@ -9,16 +9,14 @@ import (
 	"time"
 )
 
-var devicePath string
-var interval time.Duration
-var duration time.Duration
+var (
+	devicePath = flag.String("i", "/dev/input/event0", "RFID reader path")
+	interval   = flag.Duration("u", time.Minute*10, "Update interval for the chip-number database")
+	duration   = flag.Duration("t", time.Second*6, "Length of time the door should be open")
+)
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-
-	flag.StringVar(&devicePath, "i", "/dev/input/event0", "RFID reader path")
-	flag.DurationVar(&interval, "u", time.Minute*10, "Update interval for the chip number database")
-	flag.DurationVar(&duration, "t", time.Second*6, "Length of time the door should be open")
 	flag.Parse()
 }
 
@@ -26,22 +24,21 @@ func main() {
 
 	log.Printf("Doorman initialising...")
 
-	access.SetUpdateInterval(interval)
+	access.SetUpdateInterval(*interval)
 
-	log.Printf("Listening for RFID events (%s)", devicePath)
-	device := rfid.Reader(devicePath)
+	log.Printf("Listening for RFID events (%s)", *devicePath)
+	device := rfid.Reader(*devicePath)
 
 	log.Printf("----------------------------")
 	log.Printf("Doorman is ready")
 	log.Printf("----------------------------")
 
 	for id := range device.ReadIdentifiers() {
-		openDoor, name := access.HasAccess(id)
 		log.Printf("Access event: RFID=0x%08x", id)
 
-		if openDoor {
+		if ok, name := access.HasAccess(id); ok {
 			log.Printf("Open door for %s (0x%08x)", name, id)
-			door.Open(duration)
+			door.Open(*duration)
 		}
 	}
 }
