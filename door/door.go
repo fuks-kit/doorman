41bead43cefa55f1b44b2e15ec77c0e9a5d3f3f6
Patrick Zierahn <5384625+pzierahn@users.noreply.github.com>
2022-07-22 17:11:42 +0200

    Prevent double door opening.

--- door/door.go
+++ door/door.go
@@ -3,13 +3,19 @@ package door
 import (
 	"github.com/stianeikeland/go-rpio"
 	"log"
+	"sync"
 	"time"
 )
+
+var lock sync.Mutex
 
 func Open(duration time.Duration) {
 	if duration <= 0 {
 		return
 	}
+
+	lock.Lock()
+	defer lock.Unlock()
 
 	pin := rpio.Pin(26)
 
