7ebfc85062c6f09417113a715f5cd9635f922bfd
Patrick Zierahn <5384625+pzierahn@users.noreply.github.com>
2022-07-25 14:36:31 +0200

    Check in.

--- cmd/fuks/fuks.go
+++ cmd/fuks/fuks.go
@@ -21,15 +21,8 @@ func init() {
 	flag.Parse()
 }
 
-func main() {
-	log.Printf("Fetch authorised fuks users...")
-	start := time.Now()
-	users, err := fuks.GetAuthorisedFuksUsers()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Printf("--> Fetch duration %v", time.Now().Sub(start))
+func output(users []fuks.AuthorisedUser) {
+	log.Printf("--> Users %v", len(users))
 
 	sort.Slice(users, func(i, j int) bool {
 		return users[i].Name < users[j].Name
@@ -41,29 +34,30 @@ func main() {
 	}
 
 	log.Println(string(out))
+}
+
+func main() {
+	log.Printf("Fetch authorised fuks users...")
+	start := time.Now()
+	users, err := fuks.GetAuthorisedFuksUsers()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("--> Fetch duration %v", time.Now().Sub(start))
+	output(users)
 
 	if *sheetId != "" {
-		log.Printf("Fetch authorised users from sheet...")
-		start = time.Now()
-
+		log.Printf("Fetch authorised users from sheet (%s)", sheetId)
 		fuks.SetAuthUsersSheetId(*sheetId)
 
+		start = time.Now()
 		users, err = fuks.GetAuthorisedSheetUsers()
 		if err != nil {
 			log.Fatalf("cloudn't fetch users: %s", err)
 		}
 
 		log.Printf("--> Fetch duration %v", time.Now().Sub(start))
-
-		sort.Slice(users, func(i, j int) bool {
-			return users[i].Name < users[j].Name
-		})
-
-		out, err = json.MarshalIndent(users, "", "  ")
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		log.Println(string(out))
+		output(users)
 	}
 }
