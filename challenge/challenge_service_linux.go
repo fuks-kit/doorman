0c713c73a213cf5e4795043bc9625b6da68c3f8d
patrick <patrick.zierahn@student.kit.edu>
2023-11-13 18:11:38 +0100

    Move challenge to threat

--- challenge/challenge_service_linux.go
+++ challenge/challenge_service_linux.go
@@ -22,33 +22,35 @@ func StartService() error {
 
 	adv := adapter.DefaultAdvertisement()
 
-	for {
-		suffix := uuid.NewString()[8:]
-		challenge := uuid.MustParse(challengePrefix + suffix)
+	go func() {
+		for {
+			suffix := uuid.NewString()[8:]
+			challenge := uuid.MustParse(challengePrefix + suffix)
 
-		log.Printf("challenge: %s", challenge)
-		err = adv.Configure(bluetooth.AdvertisementOptions{
-			LocalName: "Doorman Nearby Challenge",
-			ServiceUUIDs: []bluetooth.UUID{
-				bluetooth.NewUUID(challenge),
-			},
-		})
-		if err != nil {
-			return err
+			log.Printf("challenge: %s", challenge)
+			err = adv.Configure(bluetooth.AdvertisementOptions{
+				LocalName: "Doorman Nearby Challenge",
+				ServiceUUIDs: []bluetooth.UUID{
+					bluetooth.NewUUID(challenge),
+				},
+			})
+			if err != nil {
+				return err
+			}
+
+			update(challenge.String())
+
+			err = adv.Start()
+			if err != nil {
+				return err
+			}
+
+			time.Sleep(refreshInterval)
+
+			err = adv.Stop()
+			if err != nil {
+				return err
+			}
 		}
-
-		update(challenge.String())
-
-		err = adv.Start()
-		if err != nil {
-			return err
-		}
-
-		time.Sleep(refreshInterval)
-
-		err = adv.Stop()
-		if err != nil {
-			return err
-		}
-	}
+	}()
 }
